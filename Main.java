c838628f2ce903701b29c8a143864a65152e6e03
Arnav <arnavbiswas2000@gmail.com>
2021-04-11 21:32:02 +0530

    java input fixed

---
+++ Main.java
@@ -0,0 +1,7 @@
+class Main{
+
+    public static void main(String[] args){
+        System.out.println("Hello World from Java !");
+        //write your code here ...
+    }
+}
---
+++ code.py
@@ -0,0 +1,5 @@
+print("Hello World !");
+
+#write your code here ...
+a = input()
+print(a)
