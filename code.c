501a7e7adb2343b4280321edf3f608fef3bf9ade
Arnav <arnavbiswas2000@gmail.com>
2021-04-11 21:02:16 +0530

    files cleaned

--- code.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main() {
-    //write your code here
-
-
-    printf("Hello World");
-    return 0;
-}
--- code.java
+++
@@ -1,6 +0,0 @@
-class Code{
-    public static void main(String[] args){
-        System.out.println("Hello World from Java !");
-        //write your code here ...
-    }
-}
